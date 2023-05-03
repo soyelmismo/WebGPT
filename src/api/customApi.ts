74058d0573de814f104fd410ddcdac3660902c21
Antonio <acheong@student.dalat.org>
2023-05-03 18:38:13 +0800

    remove unnecessary differences

--- src/api/customApi.ts
+++ src/api/customApi.ts
@@ -26,13 +26,6 @@ export const getChatCompletion = async (
   messages: MessageInterface[],
   config: ConfigInterface
 ) => {
-  var sentConfig = {
-    model: config.model,
-    temperature: config.temperature,
-    presence_penalty: config.presence_penalty,
-    top_p: config.top_p,
-    frequency_penalty: config.frequency_penalty,
-  }
 
   const response = await fetch(endpoint, {
     method: 'POST',
@@ -42,7 +35,7 @@ export const getChatCompletion = async (
     },
     body: JSON.stringify({
       messages,
-      ...sentConfig,
+      ...config,
     }),
   });
   if (!response.ok) throw new Error(await response.text());
@@ -56,13 +49,6 @@ export const getChatCompletionStream = async (
   messages: MessageInterface[],
   config: ConfigInterface
 ) => {
-  var sentConfig = {
-    model: config.model,
-    temperature: config.temperature,
-    presence_penalty: config.presence_penalty,
-    top_p: config.top_p,
-    frequency_penalty: config.frequency_penalty,
-  }
 
   const response = await fetch(endpoint, {
     method: 'POST',
@@ -72,7 +58,7 @@ export const getChatCompletionStream = async (
     },
     body: JSON.stringify({
       messages,
-      ...sentConfig,
+      ...config,
       stream: true,
     }),
   });
