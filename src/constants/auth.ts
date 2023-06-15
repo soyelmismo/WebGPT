892357c99ae371f255cc30ab69b78fccd62f2817
soyelmismo <yo@resisto.rodeo>
2023-06-15 02:29:11 -0500

    premium endpoint

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,13 +1,14 @@
-export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
+export const officialAPIEndpoint = 'https://free.churchless.tech/v1/chat/completions';
 export const defaultAPIEndpoint =
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
 
 const additionalEndpoints = [
   'https://chimeragpt.adventblocks.cc/v1/chat/completions',
-  'https://free.churchless.tech/v1/chat/completions',
+  'https://api.openai.com/v1/chat/completions',
   'https://api.pawan.krd/v1/chat/completions',
   'https://api.hypere.app/v1/chat/completions',
   'https://api.cattto.repl.co/v1/chat/completions',
+  'https://api.cattogpt.repl.co/v1/chat/completions',
 ];
 
 export const allEndpoints = [officialAPIEndpoint, ...additionalEndpoints];
