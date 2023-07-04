c2cecd33219e07b457db5c771bd0623cf01d29af
Rodeo <yo@resisto.rodeo>
2023-07-04 11:17:08 -0500

    add skailar and rearrange

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -3,13 +3,14 @@ export const defaultAPIEndpoint =
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
 
 const additionalEndpoints = [
-  'https://chimeragpt.adventblocks.cc/v1/chat/completions',
   'https://api.openai.com/v1/chat/completions',
-  'https://api.pawan.krd/v1/chat/completions',
-  'https://api.hypere.app/v1/chat/completions',
+  'https://chimeragpt.adventblocks.cc/v1/chat/completions',
   'https://api.cattto.repl.co/v1/chat/completions',
+  'https://api.skailar.net/v1/chat/completions',
+  'https://api.hypere.app/v1/chat/completions',
   'https://api.cattogpt.repl.co/v1/chat/completions',
   'https://purgpt.xyz/v1/chat/completions',
+  'https://api.pawan.krd/v1/chat/completions',
 ];
 
 export const allEndpoints = [officialAPIEndpoint, ...additionalEndpoints];
